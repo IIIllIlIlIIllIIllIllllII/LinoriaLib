fff3ab336b2eeef9cc18b3dbf9dae26c38b28bac
IIIllIlIlIIllIIllIllllII <110292384+IIIllIlIlIIllIIllIllllII@users.noreply.github.com>
2022-10-19 13:56:04 +0200

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -5,6 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
+		['Exorcism']			= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"151515","AccentColor":"aaffff","BackgroundColor":"151515","OutlineColor":"323232"}') },
 		['Tokyo'] 			= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Dracula'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
 		['Bitch Bot'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
